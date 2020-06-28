f2ac998c3a58c987d4137108b90c9c4d3a44e1c2
jstokez <jstokes@utexas.edu>
2020-06-27 22:40:39 -0500

    Full app flow, ready for code to be added

---
+++ src/Components/App/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import ApiService from '../../MiddleTier/ApiService';
+import logo from '../../logo.svg';
+import msLogo from '../../assets/microsoft_logo.png'
+import './App.css';
+
+const apiService = new ApiService();
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [timeError, setTimeError] = useState(null);
+
+  const [currentMessage, setCurrentMessage] = useState(0);
+  const [messageError, setMessageError] = useState(null);
+
+  useEffect(() => {
+    apiService.getTime()
+      .then(res => {
+        const data = res.data;
+        const time = data['time']
+        setCurrentTime(time);
+        console.log(currentTime);
+      })
+      .catch(error => {
+        setTimeError(error);
+        console.log(timeError);
+      })
+  }, []);
+
+  useEffect(() => {
+    apiService.getBase()
+      .then(res => {
+        const data = res.data;
+        console.log(data)
+        const message = data['message']
+        setCurrentMessage(message);
+      })
+      .catch(error => {
+        setMessageError(error);
+        setMessageError(messageError);
+      })
+  }, []);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={msLogo} className="App-logo" alt="logo" />
+        <p>
+          Wow look a spinning Microsoft logo
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=oHg5SJYRHA0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          What's this
+        </a>
+        <p>The current time is {currentTime}</p>
+        <p>The baseline message is {currentMessage}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
